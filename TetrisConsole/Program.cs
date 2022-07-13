1da5ca50a9533d374e825277ae4d5762ae924696
Peter Chase <peter.chase@willistowerswatson.com>
2022-07-13 10:59:27 +0100

    Program now writes the timer and movements to console

--- TetrisConsole/Program.cs
+++ TetrisConsole/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using TetrisLib;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        ConsoleKeyMonitor.Start();
+        using var finished = new Subject<Unit>();
+
+        var playerMoves = ConsoleKeyMonitor.Movements.Finally(() => finished.OnNext(Unit.Default));
+
+        var timerCounts = Observable.Interval(TimeSpan.FromSeconds(1)).TakeUntil(finished);
+
+        var t2 = playerMoves.ForEachAsync(async m => await Console.Out.WriteLineAsync(m.ToString()));
+        var t1 = timerCounts.ForEachAsync(async c => await Console.Out.WriteLineAsync(c.ToString()));
+
+        await Task.WhenAll(t1, t2);
+    }
+}
