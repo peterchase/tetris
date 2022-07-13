6c714e93ee1d81aa40c1d557912eda71df7b4dcf
Peter Chase <peter.chase@willistowerswatson.com>
2022-07-13 18:49:44 +0100

    One more shape

--- TetrisLib/StandardShapes.cs
+++ TetrisLib/StandardShapes.cs
@@ -6,8 +6,9 @@ public static class StandardShapes
 {
     public static readonly Shape L42 = new(new Rectangle(0, 0, 4, 1), new Rectangle(0, 0, 1, 2));
     public static readonly Shape Offset32 = new(new Rectangle(0, 0, 3, 1), new Rectangle(1, 1, 3, 1));
-    public static readonly Shape Square33 = new(new Rectangle(0, 0, 3, 3));
+    public static readonly Shape Square32 = new(new Rectangle(0, 0, 3, 2));
     public static readonly Shape U23 = new(new Rectangle(0, 0, 2, 1), new Rectangle(0, 1, 1, 1), new Rectangle(0, 2, 2, 1));
+    public static readonly Shape T23 = new(new Rectangle(1, 0, 1, 3), new Rectangle(0, 1, 2, 1));
 
     public static IEnumerable<Shape> All
     {
@@ -15,8 +16,9 @@ public static class StandardShapes
         {
             yield return L42;
             yield return Offset32;
-            yield return Square33;
+            yield return Square32;
             yield return U23;
+            yield return T23;
         }
     }
 }
