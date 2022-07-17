35c98bf11cf215f232c82f8da0e66c9bd93bc1e9
Peter Chase <pete@peterchase.me>
2022-07-17 13:58:00 +0100

    Introduce constants in Program

--- TetrisConsole/Program.cs
+++ TetrisConsole/Program.cs
@@ -8,6 +8,10 @@ using TetrisLib;
 
 internal class Program
 {
+    private const double cInitialDelayMillis = 500.0, cMinDelayMillis = 25.0;
+    private const double cDelayReductionFactor = 0.995;
+    private const int cBoardWidth = 20, cBoardHeight = 20;
+
     private static async Task Main(string[] args)
     {
         ConsoleKeyMonitor.Start();
@@ -18,17 +22,17 @@ internal class Program
         var playerMoves = ConsoleKeyMonitor.Moves.Finally(() => finished.OnNext(Unit.Default));
 
         var timerCounts = Observable
-            .Generate(500.0, _ => true, GetFaster, d => (long)d, d => TimeSpan.FromMilliseconds(d))
+            .Generate(cInitialDelayMillis, _ => true, GetFaster, d => (long)d, d => TimeSpan.FromMilliseconds(d))
             .TakeUntil(finished);
 
         var initialMovingPiece = new Piece(StandardShapes.L42, new Point(2, 0));
 
-        var initialBoard = new Board(new Size(20, 20)) { MovingPiece = initialMovingPiece };
+        var initialBoard = new Board(new Size(cBoardWidth, cBoardHeight)) { MovingPiece = initialMovingPiece };
         var game = new Game(timerCounts, playerMoves, initialBoard, new StandardRules(StandardShapes.All));
 
         var builder = new StringBuilder();
         await game.Boards.ForEachAsync(async b => await Console.Out.WriteAsync(b.ToConsoleString(builder)));
     }
 
-    private static double GetFaster(double prevDelay) => Math.Max(25.0, prevDelay * 0.995);
+    private static double GetFaster(double prevDelay) => Math.Max(cMinDelayMillis, prevDelay * cDelayReductionFactor);
 }
